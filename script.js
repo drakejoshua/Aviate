5af5bd53508b7dd0c3dd9224dbbb048988258a2e
Joshua Mabawonku <drakejoshua086@gmail.com>
2023-12-23 13:25:37 +0100

    feat: added parallax using jarallax
    - added the parallax effect to the section of the webpage
      using the jarallax.js library.

---
+++ script.js
@@ -0,0 +1,10 @@
+var slowJarallaxElements = document.getElementsByClassName("jarallax");
+var fastJarallaxElements = document.getElementsByClassName("fastJarallax");
+
+jarallax( slowJarallaxElements, {
+    speed: 0.3
+});
+
+jarallax( fastJarallaxElements, {
+    speed: 0.1
+});
